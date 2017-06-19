f9aeb8fd85aec610950f0cef9c32c4011fae605a
Dramelac <203697@supinfo.com>
2017-06-20 00:46:52 +0200

    Cleanup using status

--- Winplex/Winplex/MainPage.xaml.cs
+++ Winplex/Winplex/MainPage.xaml.cs
@@ -1,41 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Devices.Geolocation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 using Winplex.models;
 using Winplex.Utils;
-
 
 namespace Winplex
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    ///     An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public MainPage()
+        {
+            InitializeComponent();
+            NextDays = new ObservableCollection<WeatherDayVM>();
+            Page_Loaded();
+        }
+
         private Geolocalisation Geolocalisation { get; set; }
 
         public ObservableCollection<WeatherDayVM> NextDays { get; set; }
-
-        public MainPage()
-        {
-            this.InitializeComponent();
-            this.NextDays = new ObservableCollection<WeatherDayVM>();
-            this.Page_Loaded();
-        }
 
         public async void Page_Loaded()
         {
@@ -49,59 +38,61 @@ namespace Winplex
                     pos.Coordinate.Point.Position.Latitude,
                     pos.Coordinate.Point.Position.Longitude);*/
 
-                Weather_API data = await OpenWeatherAPI.GetWeatherData(pos.Coordinate.Point.Position.Latitude,
+                var data = await OpenWeatherAPI.GetWeatherData(pos.Coordinate.Point.Position.Latitude,
                     pos.Coordinate.Point.Position.Longitude);
-                this.LoadData(data);
+                LoadData(data);
             }
             else
             {
-                this.FailGeoloc();
+                FailGeoloc();
             }
         }
 
         public void FailGeoloc()
         {
-            this.MainTitle.Text = "Location is not available. Please choose a city.";
+            MainTitle.Text = "Location is not available. Please choose a city.";
         }
 
         public void LoadData(Weather_API data)
         {
-            this.MainTitle.Text = "Today";
+            MainTitle.Text = "Today";
 
-            this.MainCity.Text = data.city.name;
-            this.MainWeather.Text = data.list.ElementAt(0).weather.ElementAt(0).main;
-            this.MainDesc.Text = data.list.ElementAt(0).weather.ElementAt(0).description;
-            this.MainDeg.Text = Weather_Main.KelvinToCelsiusString(data.list.ElementAt(0).main.temp);
+            MainCity.Text = data.city.name;
+            MainWeather.Text = data.list.ElementAt(0).weather.ElementAt(0).main;
+            MainDesc.Text = data.list.ElementAt(0).weather.ElementAt(0).description;
+            MainDeg.Text = Weather_Main.KelvinToCelsiusString(data.list.ElementAt(0).main.temp);
 
             //Weather Image
-            string icon = string.Format("ms-appx:///Assets/icon/{0}.png", data.list.ElementAt(0).weather.ElementAt(0).icon);
-            this.MainImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+            var icon = string.Format("ms-appx:///Assets/icon/{0}.png", data.list.ElementAt(0).weather.ElementAt(0).icon);
+            MainImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
 
             //Wind arrow
-            this.MainWind.Visibility = Visibility.Visible;
-            this.MainWind.RenderTransform = new RotateTransform { Angle = data.list.ElementAt(0).wind.deg, CenterX = 50, CenterY = 50};
-            this.MainWindSpeed.Text = "Wind speed : " + data.list.ElementAt(0).wind.speed + " m/s";
+            MainWind.Visibility = Visibility.Visible;
+            MainWind.RenderTransform = new RotateTransform
+            {
+                Angle = data.list.ElementAt(0).wind.deg,
+                CenterX = 50,
+                CenterY = 50
+            };
+            MainWindSpeed.Text = "Wind speed : " + data.list.ElementAt(0).wind.speed + " m/s";
 
-            this.LoadNextDays(data.list);
+            LoadNextDays(data.list);
         }
 
         public void LoadNextDays(List_Weather_API[] data)
         {
-            this.NextDays.Clear();
+            NextDays.Clear();
 
-            this.NextDays.Add(WeatherDayVM.ObjectToVm(data[8]));
-            this.NextDays.Add(WeatherDayVM.ObjectToVm(data[16]));
-            this.NextDays.Add(WeatherDayVM.ObjectToVm(data[24]));
-            this.NextDays.Add(WeatherDayVM.ObjectToVm(data[32]));
-
-            //Test.ItemsSource = this.NextDays;
-
+            NextDays.Add(WeatherDayVM.ObjectToVm(data[8]));
+            NextDays.Add(WeatherDayVM.ObjectToVm(data[16]));
+            NextDays.Add(WeatherDayVM.ObjectToVm(data[24]));
+            NextDays.Add(WeatherDayVM.ObjectToVm(data[32]));
         }
 
         private async void FindPostalCode(object sender, RoutedEventArgs e)
         {
             var result = await OpenWeatherAPI.GetWeatherDataFromZipCode(PostalCodeInput.Text);
-            if (result != null) this.LoadData(result);
+            if (result != null) LoadData(result);
         }
     }
 }
