deefedabcb6bdb9ebb09ace97b10b42d9fefc9c2
Dramelac <203697@supinfo.com>
2017-06-19 14:25:43 +0200

    Create project

---
+++ Winplex/Winplex/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Winplex.Utils;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Winplex
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Geolocalisation Geolocalisation { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.InitGeoStatus();
+
+        }
+
+        public async void InitGeoStatus()
+        {
+            Geolocalisation = new Geolocalisation();
+            await Geolocalisation.Init();
+            var pos = await Geolocalisation.GeoPosition();
+            GeolocStatus.Text = "Geolocalisation: " + Geolocalisation.Status;
+            Coord.Text = String.Format("Lat: {0}, Long: {1}, {2}", 
+                pos.Coordinate.Point.Position.Latitude, 
+                pos.Coordinate.Point.Position.Longitude,
+                pos.CivicAddress.PostalCode);
+        }
+    }
+}
---
+++ Winplex/Winplex/Utils/Geolocalisation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace Winplex.Utils
+{
+    class Geolocalisation
+    {
+        private Geolocator _geolocator { get; set; }
+
+        public string Status { get; set; }
+
+        public Geolocalisation()
+        {
+            this.Status = "No initialized";
+        }
+
+        public async Task Init()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+
+                    // If DesiredAccuracy or DesiredAccuracyInMeters are not set (or value is 0), DesiredAccuracy.Default is used.
+                    _geolocator = new Geolocator { DesiredAccuracyInMeters = 0 };
+                    this.Status = "Enable";
+                    break;
+
+                case GeolocationAccessStatus.Denied:
+                    this.Status = "Denied";
+                    break;
+
+                case GeolocationAccessStatus.Unspecified:
+                    this.Status = "Unknown";
+                    break;
+            }
+            
+        }
+
+        
+
+        public async Task<Geoposition> GeoPosition()
+        {
+            if (_geolocator != null)
+            {
+                return await _geolocator.GetGeopositionAsync();
+            }
+            return null;
+        }
+    }
+}
